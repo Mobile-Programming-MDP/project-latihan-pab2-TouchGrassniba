007b791f87748b74faabb1a9e81a18ac2d7b4248
TouchGrassniba <valentinojuanito56@gmail.com>
2025-04-23 19:52:18 +0700

    p

--- fasum_app/lib/screens/home_screen.dart
+++ fasum_app/lib/screens/home_screen.dart
@@ -3,16 +3,30 @@ import 'package:fasum_app/screens/sign_up_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  Future<void> signOut(BuildContext context) async{
+  String formatTime(DateTime dateTime) {
+    final now = DateTime.now();
+    final diff = now.difference(dateTime);
+    if (diff.inSeconds < 60) {
+      return '${diff.inSeconds} sec ago';
+    } else if (diff.inMinutes < 60) {
+      return '${diff.inSeconds} mins ago';
+    } else if (diff.inHours < 24) {
+      return '${diff.inSeconds} hrs ago';
+    } else {
+      return DateFormat('dd/MM/yyyy').format(dateTime);
+    }
+  }
+
+  Future<void> signOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
     Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: (context) => const SignInScreen()
-      )
+      MaterialPageRoute(builder: (context) => const SignInScreen()),
     );
   }
 
@@ -23,17 +37,15 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Home'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         actions: [
-        IconButton(
-          onPressed: (){
-            signOut(context);
-          }, 
-          icon: const Icon(Icons.logout)
-          )
+          IconButton(
+            onPressed: () {
+              signOut(context);
+            },
+            icon: const Icon(Icons.logout),
+          ),
         ],
       ),
-      body: const Center(
-        child: const Text('You have logged in'),
-      ),
+      body: const Center(child: const Text('You have logged in')),
     );
   }
 }
